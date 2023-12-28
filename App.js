76cf164c5ae906bc8b1d280451b96f6628f0db52
kkamara <kamaracomputers@gmail.com>
2023-12-28 19:09:33 +0000

    Handling events

--- App.js
+++ App.js
@@ -5,15 +5,29 @@ import {
   TextInput,
   Button,
 } from "react-native"
+import { useState, } from "react"
 
 export default function App() {
+  const [value, setValue] = useState("")
+  
+  function handleOnChangeText(getEnteredText) {
+    setValue(getEnteredText)
+  }
+  
+  function handleOnPressButton() {
+    console.log(value)
+  }
+
   return (
     <View style={{
       padding: 60,
     }}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Add your note here"/>
-        <Button color={"#000"} title="Add note" />
+        <TextInput onChangeText={handleOnChangeText} style={styles.input} placeholder="Add your note here"/>
+        <Button onPress={handleOnPressButton} color={"#000"} title="Add note" />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>Show lists here.</Text>
       </View>
     </View>
   )
@@ -22,10 +36,15 @@ export default function App() {
 const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: "row",
+    paddingBottom: 30,
+    borderBottomWidth: 1,
   },
   input: {
     borderWidth: 1,
     borderColor: "#ddd",
     flex: 1,
+  },
+  listContainer: {
+    paddingTop: 30,
   }
 })
